81b03b58dbf1d843777b9bb9f21f803a8db56b6d
Daniel Wood <dzwood327@gmail.com>
2019-06-11 11:32:43 -0400

    Update FooterTesting.js

--- src/components/Page/FooterTesting.js
+++ src/components/Page/FooterTesting.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'reactstrap'
 import { Accordion, Loader, Content } from '@christfellowshipchurch/flat-ui-web'
 import Footer from '../Footer'
 import Navbar from '../Navbar'
+import Grid from '../Grid'
 
 const DefaultPage = () => {
    const demoLinks = [
@@ -11,13 +12,24 @@ const DefaultPage = () => {
     {call: 'Another Item',
     action: '/anotheritem' },
   ]   
+  const demoRegister = [
+    {call: 'Register Now',
+    action: '/registration'}
+  ]
 
   const demoIcon = 'https://cdn4.iconfinder.com/data/icons/new-google-logo-2015/400/new-google-favicon-512.png'
 
   return (
     <div>
-      <Navbar navLinks={demoLinks} brandImage={demoIcon}></Navbar>
+      <Navbar navLinks={demoLinks} brandImage={demoIcon} quickAction={demoRegister}></Navbar>
+          <Grid title="This is a Title" body={<div>here you can add body text</div>} backgroundImg="https://data.whicdn.com/images/67020789/original.jpg" backgroundColor="white">
+            <Loader.Content />
+            <Loader.Content />
+            <Loader.Content />
+            <Loader.Content />
+            <Loader.Content />
 
+          </Grid>
       <Footer linkstoPages={demoLinks} brandIcon={demoIcon}></Footer>
     </div>
   )
