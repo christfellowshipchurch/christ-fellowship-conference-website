7ea3367e79ffe90392090d7c1c896ac323b4c6ce
Daniel Wood <dzwood327@gmail.com>
2019-06-11 11:32:38 -0400

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import classnames from 'classnames'
 import { Container, Row, Col, Nav, NavItem, NavLink, Button, UncontrolledCollapse } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 
@@ -14,21 +12,25 @@ const Navbar = ({ brandImage, quickAction, navLinks }) => {
         width: '50px'
     }
 
+    const navStyle = {
+        fontSize: '35px',
+        fontFamily: '"Arial Black", sans-serif'
+    }
+
+
+
     return (
         <div>
-            <Nav vertical className="navbar navbar-expand-md bg-dark navbar-dark">
-                <NavItem className="d-flex justify-content-around">
-                    <img className='p-2' src={brandImage} style={brandStyle} href='#'></img>
-                    <h3 className='p-2 text-white'> Register Now</h3>
-                    <FontAwesomeIcon id="toggler" icon={faBars} size='3x' color="white" className='p-2'></FontAwesomeIcon>
-                </NavItem>
-                <UncontrolledCollapse toggler="#toggler">
-                    <NavItem>
-                        <NavLink href='#'>
-                            This is a link item
-                        </NavLink>
-                    </NavItem>
-                        {navLinks.map((link) => <NavItem><NavLink href={link.action}>{link.call}</NavLink></NavItem>)}
+            <Nav vertical className="bg-dark">
+            <div className="d-flex bg-dark">
+                <div className='py-3 mr-auto ml-3'><img src={brandImage} style={brandStyle} href='#'></img></div>
+                {console.log(quickAction)}
+                <div>{quickAction.map((link) => <h5 className='py-4 text-white text-uppercase' href={link.action}>{link.call}</h5>)}</div>
+                <div className='mr-3'><FontAwesomeIcon id="toggler" icon={faBars} size='5x' color="white" className='px-3 py-4'></FontAwesomeIcon></div>
+            </div>
+            {console.log(navLinks)}
+                <UncontrolledCollapse toggler="#toggler" className='p-5'>
+                    {navLinks.map((link) => <NavItem><NavLink style={navStyle} href={link.action} className='text-uppercase text-white'>{link.call}</NavLink></NavItem>)}
                 </UncontrolledCollapse>
             </Nav>
         </div>
