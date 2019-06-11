099944a3b249d464ad95d2c113d6f9ef61041504
Caleb Panza <caleb.panza@icloud.org>
2019-06-11 17:17:11 -0400

    Minor updates and refactoring

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,28 +6,30 @@ import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg
 
 
 // body is a react component
-const Footer = ({instagramUrl, facebookUrl, twitterUrl, brandIcon, linkstoPages}) => {
+const Footer = ({ instagramUrl, facebookUrl, twitterUrl, brandIcon, linkstoPages }) => {
 
-return(
-    <Container fluid className='bg-dark'>
-        <Row className='m-auto'>
-            <Col xs="10" className="d-flex align-items-start flex-column p-3 px-5">
-                <div>
-                    <a href={instagramUrl}><FontAwesomeIcon color='white' icon={faInstagram} size='2x'className='mr-3'></FontAwesomeIcon></a>
-                    <a href={facebookUrl}><FontAwesomeIcon color='white' icon={faTwitter} size='2x'className='mr-3'></FontAwesomeIcon></a>
-                    <a href={twitterUrl}><FontAwesomeIcon color='white' icon={faFacebook} size='2x'className='mr-3'></FontAwesomeIcon></a>
-                </div>
-                <p className='mb-0 font-weight-light'>©2019 Christ Fellowship Church. All Rights Reserved</p>
-                <div className='d-flex'>
-                {linkstoPages.map((link) => <a href={link.action} className='pr-3 text-white font-weight-bold'>{link.call}</a>)}
-                </div>
+    return (
+        <Container fluid className='bg-dark'>
+            <Row className='m-auto align-items-center'>
+                <Col xs="12" md="10" className="text-left py-3">
+                    <div>
+                        <a href={instagramUrl}><FontAwesomeIcon color='white' icon={faInstagram} size='2x' className='mr-3'></FontAwesomeIcon></a>
+                        <a href={facebookUrl}><FontAwesomeIcon color='white' icon={faTwitter} size='2x' className='mr-3'></FontAwesomeIcon></a>
+                        <a href={twitterUrl}><FontAwesomeIcon color='white' icon={faFacebook} size='2x' className='mr-3'></FontAwesomeIcon></a>
+                    </div>
+                    <p className='mb-0 font-weight-light'>&copy;{new Date().getFullYear()} Christ Fellowship Church. All Rights Reserved</p>
+                    <div className='d-flex'>
+                        {linkstoPages.map((link) => <a href={link.action} className='pr-3 text-white font-weight-bold'>{link.call}</a>)}
+                    </div>
                 </Col>
-            <Col xs="2" className='d-flex justify-content-center' >
-                <img style={{height:'100px', width:'100px'}} src={brandIcon} alt="brand icon" className='align-self-center'></img>
-            </Col>
-        </Row>
-    </Container>
-)
+                <Col xs="12" md="2" className='py-3'>
+                    <a href="/">
+                        <img alt="Christ Fellowship Conference" style={{ height: '100px', width: 'auto' }} src={brandIcon} alt="brand icon" className='align-self-center'></img>
+                    </a>
+                </Col>
+            </Row>
+        </Container>
+    )
 
 
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,18 +5,34 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 
 // body is a react component
-const Navbar = ({ brandImage, quickAction, navLinks}) => {
+const Navbar = ({ brandImage, quickAction, navLinks }) => {
 
 
 
     return (
         <div className='fixed-top'>
             <Nav vertical className="bg-dark">
-            <div className="d-flex bg-dark">
-                <div className='py-3 mr-auto ml-3'><img src={brandImage} style={{height: '50px', width: '50px'}} href='#'></img></div>
-                <div>{quickAction.map((link) => <h5><NavLink href={link.action} className='py-4 text-white text-uppercase font-weight-bold'>{link.call}</NavLink></h5>)}</div>
-                <div className='mr-3'><FontAwesomeIcon id="toggler" icon={faBars} size='5x' color="white" className='px-3 py-4'></FontAwesomeIcon></div>
-            </div>
+                <div className="d-flex bg-dark">
+                    <div className='py-3 mr-auto ml-3'>
+                        <a href="/">
+                            <img src={brandImage} style={{ height: '50px', width: '50px' }} href='#' alt="Christ Fellowship Conference" />
+                        </a>
+                    </div>
+                    <div>
+                        {quickAction.map((link) => (
+                            <h5>
+                                <NavLink
+                                    href={link.action}
+                                    className='py-4 text-white text-uppercase font-weight-bold'>
+                                    {link.call}
+                                </NavLink>
+                            </h5>
+                        ))}
+                    </div>
+                    <div className='mr-3'>
+                        <FontAwesomeIcon id="toggler" icon={faBars} size='5x' color="white" className='px-3 py-4'></FontAwesomeIcon>
+                    </div>
+                </div>
                 <UncontrolledCollapse toggler="#toggler" className='p-5'>
                     {navLinks.map((link) => <NavItem><h1><NavLink href={link.action} className='text-white text-uppercase font-weight-bold'>{link.call}</NavLink></h1></NavItem>)}
                 </UncontrolledCollapse>
