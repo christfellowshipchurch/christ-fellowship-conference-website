85f9f0718b8311d7129ebc1028baffcea067ea5d
Caleb Panza <caleb.panza@icloud.org>
2019-06-12 11:52:58 -0400

    Added Conference Date to Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
-import { hero, heroImg } from '../../styles/hero.module.css'
+import { hero, heroImg, heroContainer } from '../../styles/hero.module.css'
 import titleImg from './Christ-Fellowship-Church-Conference-2020.png'
 import yearImg from './Christ-Fellowship-Conference-February-2020.png'
 
@@ -9,8 +9,17 @@ const Hero = ({ hide = false }) => hide
     ? null
     : (
         <div className={hero}>
-            <img className={heroImg} src={titleImg} alt="Christ Fellowship Church Conference" />
-            <img className={heroImg} src={yearImg} alt="Christ Fellowship Church Conference" />
+
+            <div className={heroContainer}>
+                <img className={heroImg} src={titleImg} alt="Christ Fellowship Church Conference" />
+                <img className={heroImg} src={yearImg} alt="Christ Fellowship Church Conference" />
+            </div>
+
+            <div className={heroContainer}>
+                <h1 className="font-weight-bold text-uppercase align-self-end text-light">
+                    Feb 12-13 • West Palm Beach, FL
+                </h1>
+            </div>
         </div>
     )
 
