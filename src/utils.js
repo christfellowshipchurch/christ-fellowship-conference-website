04c114513e18c01e739f20e5e5eb6c0b6274e24b
Daniel Wood <dzwood327@gmail.com>
2019-06-26 13:06:03 -0400

    Added New tab Functionality to Buttons
    Users in Rock can now set whether or not they want a button to open link in new tab

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,6 @@ import { Content, Media } from '@christfellowshipchurch/flat-ui-web'
 import { Container, Row, Col, Button } from 'reactstrap'
 import PixelManager from './components/PixelManager'
 
-
 export const mapEdgesToNodes = (data) => data.edges.map(n => n.node);
 export const redirectTo = (uri) => window.location.href = uri;
 
@@ -89,7 +88,6 @@ export const renderContent = (content) => {
                     </Row>
                 </Container>
             </Container>
-
         )
     }
 }
@@ -145,18 +143,25 @@ const renderContentWithImgSizing = (content) => {
                     {content.htmlContent}
                 </Content.Body>
 
-                {renderButtons(content.callsToAction, content.buttonColor, content.title)}
+                {renderButtons(content.callsToAction, content.buttonColor, content.title, content.openLinksInNewTab)}
             </Content>
         </div>
     )
 }
 
-export const buttonClick = (call, action, title) => {
+export const buttonClick = (call, action, title, openLinksInNewTab) => {
     PixelManager.reportButtonClick({ call: `${title} - ${call}`, action })
-    redirectTo(action)
+    // const lowerCall = lowerCase(call)
+    if(openLinksInNewTab){
+        const win = window.open(action,'_blank')
+        win.focus()
+    }
+    else { 
+        redirectTo(action)
+    }
 }
 
-export const renderButtons = (callsToAction, buttonColor, title) => (
+export const renderButtons = (callsToAction, buttonColor, title, openLinksInNewTab) => (
     <Container className="px-0">
         {callsToAction.map((n, i) => {
             const styles = {
@@ -166,7 +171,8 @@ export const renderButtons = (callsToAction, buttonColor, title) => (
             return (
                 <Row className="my-2" key={i}>
                     <Col size="12">
-                        <Button style={styles} onClick={() => buttonClick(n.call, n.action, title)}>
+                        <Button style={styles} onClick={() => buttonClick(n.call, n.action, title, openLinksInNewTab)}>
+                        {console.log(openLinksInNewTab)}
                             {n.call}
                         </Button>
                     </Col>
